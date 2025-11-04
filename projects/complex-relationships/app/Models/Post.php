b672442a470b2d9ab0e8387bfcf6fd91afcf8301
Bunga-hub <bungaalfazahrah@gmail.com>
2025-11-04 16:00:48 +0700

    complex-relationship

---
+++ projects/complex-relationships/app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * Kolom yang dapat diisi secara massal
+     */
+    protected $fillable = ['user_id', 'title', 'content'];
+
+    /**
+     * Relasi inverse One-to-Many dengan User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Relasi Many-to-Many dengan Tag
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
